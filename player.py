dd5853c121318dff6b84665623371042b6d49dc3
roboticmonkey <aiden.ward@gmail.com>
2017-02-15 11:06:41 -0800

    starting

---
+++ player.py
@@ -0,0 +1,12 @@
+
+class Player(object):
+    """Player class"""
+
+    def __init__(self, name):
+        self.name = name
+        
+
+
+
+
+
---
+++ wordbook.py
@@ -0,0 +1,7 @@
+import requests
+import random
+
+
+class Wordbook(object):
+    """Class to create words used in Hangman game"""
+    
