ac7e1120cd2fa80b65ce73e1dc0a9d4c70779538
roboticmonkey <aiden.ward@gmail.com>
2017-02-15 12:30:06 -0800

    added request_letter funct

--- utilities.py
+++ utilities.py
@@ -2,3 +2,18 @@
 def print_word(word):
     """Takes a list of chars and prints as a string"""
     print ''.join(word)
+
+def request_letter():
+    """ Requests a letter and Returns a valid lowercase letter between
+        a-z"""
+    a = unicode('a')
+    z = unicode('z')
+    invalid = True
+
+    while invalid:
+        letter = unicode(raw_input("Enter a Letter: ")).lower()
+
+        if letter >= a and letter <= z:
+            invalid = False
+
+    return letter
