446d0e37a98045ea897e75c1ccfd52d2bc8c71c6
roboticmonkey <aiden.ward@gmail.com>
2017-02-15 13:09:30 -0800

    added find_letter_in_word and update_guess function

--- utilities.py
+++ utilities.py
@@ -17,3 +17,25 @@ def request_letter():
             invalid = False
 
     return letter
+
+def find_letter_in_word(letter, secret_word):
+    """ Looks for letter in word
+        Returns [] of matching indexes, Empty [] = Not found """
+
+    index = []
+
+    for i, char in enumerate(secret_word):
+        if char == letter:
+            index.append(i)
+
+    return index
+
+def update_guess(indexes, letter, guess):
+    """ Replaces '_' with letter at indexes of guess 
+        Returns updated guess """
+
+    for i in indexes:
+        guess[i] = letter
+
+    return guess
+
