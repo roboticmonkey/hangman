0c7cb17822dbcce584ee5700ab4add302a4f5f60
roboticmonkey <aiden.ward@gmail.com>
2017-02-17 13:21:37 -0800

    deleted some old code.

--- model.py
+++ model.py
@@ -23,21 +23,7 @@ class Wordbook(db.Model):
     def __init__(self, book):
         """creates Wordbook object"""
         self.book = book
-        
-
-    # def select_word(self):
-    #     """ Selects a psudo-random word from the book
-    #         Returns a list object of word. ie. 'ant' = ['a','n','t']"""
-
-    #     rand_num = random.randint(0, len(self.book)-1)
-
-    #     while rand_num in self.indexes:
-    #         rand_num = random.randint(0, len(self.book)-1)
-
-    #     self.indexes.add(rand_num)
-    #     secret_word = list(self.book[rand_num])
-
-    #     return secret_word
+   
 
 class Used_index(db.Model):
     """indexes used for accessing words from dictionary"""
