901144b6587a4c12a73bd7f678d0fbe3da493333
roboticmonkey <aiden.ward@gmail.com>
2017-02-17 09:55:30 -0800

    trying to create the wed ui

--- server.py
+++ server.py
@@ -6,6 +6,7 @@ from flask_debugtoolbar import DebugToolbarExtension
 
 import hangman
 import utilities as util
+from model import connect_to_db, Wordbook, db
 
 
 app = Flask(__name__)
@@ -27,6 +28,28 @@ def start_game():
     game.new_game()
     num = game.num_guesses
     guess = util.convert_to_string(game.guess)
+
+    dic = Wordbook()
+    dic.create_wordbook()
+    new_dic = Wordbook(book=dic.book)
+    db.session.add(new_dic)
+    db.session.commit()
+    session['id'] = new_dic.wordbook_id
+
+    words = Wordbook.query.filter_by(wordbook_id=new_dic.wordbook_id).first()
+    words.indexes = [1,5,7]
+    db.session.commit()
+    temp = words.indexes
+    for each in temp:
+        print each
+        print type(each)
+
+    # temp.append(6)
+    
+    # words.indexes = temp
+    # db.session.commit()
+
+
     
 
 
@@ -63,7 +86,7 @@ if __name__ == "__main__":
     app.debug = True
     # app.debug = False
 
-    # connect_to_db(app)
+    connect_to_db(app)
 
     # Use the DebugToolbar
     DebugToolbarExtension(app)
