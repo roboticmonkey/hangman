14e016d0d5c40c23fe2f85ff237a6683d67dcd60
roboticmonkey <aiden.ward@gmail.com>
2017-02-18 17:02:09 -0800

    added comments, some doctests

--- server.py
+++ server.py
@@ -20,6 +20,9 @@ app.jinja_env.undifined = StrictUndefined
 
 @app.route('/')
 def index():
+    """Renders the homepage"""
+
+    # Sets a bunch of session variables to none
     session['num_left'] = None
     session['letters'] = []
     session['guess'] =[]
@@ -29,9 +32,14 @@ def index():
 
 @app.route('/start_game', methods=['POST'])
 def start_game():
+    """Sets up the game and renders the gamepage"""
     
+    # Gets the difficulty setting
     difficulty = request.form.get('difficulty')
+    
+    # Number of chances to guess the word
     num = 6
+
     # set/reset session vars 
     session['num_left'] = num
 
@@ -62,13 +70,15 @@ def start_game():
 
 @app.route('/new_word')
 def new_word():
-    
+    """Selects a new word from the already generated wordbook
+        renders the gamepage"""
+
+
     num = 6
     # set/reset session vars 
     session['num_left'] = num
     session['letters'] = []
     session['guess'] =[]
-    # session['id'] = None
     session['secret_word'] = []
 
     # Retrive wordbook from db
@@ -90,18 +100,26 @@ def new_word():
 
 @app.route('/take_turn', methods=['POST'])
 def playing_game():
+    """Handles the playing of the game"""
+
+    # Retrives session variables
     num = session['num_left']
     secret_word = session['secret_word']
     guess = session['guess']
     
     letters = session['letters']
     
+    #c Checks if the Game is over
     if not game_over(num, secret_word, guess):
 
+        # Gets the data from the form
         letter = request.form.get('letter')
         letter = clean_data(letter)
         
-        if is_dupilicate(letter, letters):
+        # Checks if player has entered that guess before
+        if is_duplicate(letter, letters):
+
+            # If dupicate guess, asks them to try again
             flash("You already tried that one.")
             guess_word = convert_to_string(guess)
             
@@ -110,7 +128,10 @@ def playing_game():
                             missed=letters,
                             num_guesses=num)
 
+        # Checks if player has only entered letters
         if not letter.isalpha():
+
+            # If play entered non letters asks them to try again
             flash("You must enter only letters.")
             guess_word = convert_to_string(guess)
             
@@ -119,7 +140,8 @@ def playing_game():
                             missed=letters,
                             num_guesses=num)
         
-       
+       # If player enters more that 1 letter
+       # Checks if they guessed the word correctly
         if len(letter) > 1:
             if words_match(secret_word, letter):
                 guess = letter
@@ -133,25 +155,29 @@ def playing_game():
                                 win=winner,
                                 over=over)
             else:
-
+                # Wrong guess, lose a chance
                 num -= 1
                 session['num_left'] = num
         else:
 
+            # If player only guesses 1 letter
             indexes = find_letter_in_word(secret_word, letter)
 
+            # If letter matches found, update the guess word
             if indexes:
                 guess = update_guess(guess,indexes, letter)
                 session['guess'] = guess
                 
-
+            # No match, lose a chance
             else:
                 num -= 1
                 session['num_left'] = num
-            
+        
+        # Adds guess to list, updates session var
         letters.append(letter)
         session['letters'] = letters
 
+    # Varibles needed for template rendering
     guess_word = convert_to_string(guess)
 
     over = game_over(num, secret_word, guess)
@@ -208,13 +234,36 @@ def get_used_indexes(wordbook):
     return used
 
 def convert_to_string(char_list):
-    """Takes a list of chars and returns as a string"""
+    """Takes a list of chars and returns as a string
+
+        >>> a = ['d','o','g']
+        >>> convert_to_string(a)
+        'dog'
+    """
     
     return ''.join(char_list)
 
 def game_over(num_guesses, secret_word, guess):
     """Checks if game is over. Returns True if Over.
-        False if not Over."""
+        False if not Over.
+
+        >>> num = 0
+        >>> secret = ['d','o','g']
+        >>> guess = ['d', '_','_']
+        >>> game_over(num, secret, guess)
+        True
+        >>> num = 4
+        >>> secret = ['d','o','g']
+        >>> guess = ['d', 'o','g']
+        >>> game_over(num, secret, guess)
+        True
+        >>> num = 4
+        >>> secret = ['d','o','g']
+        >>> guess = ['d', '_','_']
+        >>> game_over(num, secret, guess)
+        False
+
+        """
 
     if num_guesses > 0 and secret_word != guess:
         return False
@@ -223,7 +272,20 @@ def game_over(num_guesses, secret_word, guess):
 
 def is_winner(num_guesses, secret_word, guess):
     """Checks if Player won. Returns True if Won.
-        False if lost."""
+        False if lost.
+
+        >>> num = 4
+        >>> secret = ['d','o','g']
+        >>> guess = ['d', 'o','g']
+        >>> is_winner(num, secret, guess)
+        True
+        >>> num = 0
+        >>> secret = ['d','o','g']
+        >>> guess = ['d', '_','_']
+        >>> is_winner(num, secret, guess)
+        False
+
+        """
 
     if num_guesses > 0 and secret_word == guess:
         return True
@@ -232,7 +294,18 @@ def is_winner(num_guesses, secret_word, guess):
 
 def find_letter_in_word(secret_word, letter):
     """ Looks for letter in secret_word
-        Returns [] of matching indexes, Empty [] = None found """
+        Returns [] of matching indexes, Empty [] = None found
+
+        >>> secret = ['a','p','p','l','e']
+        >>> letter = 'p'
+        >>> find_letter_in_word(secret, letter)
+        [1, 2]
+        >>> secret = ['a','p','p','l','e']
+        >>> letter = 'y'
+        >>> find_letter_in_word(secret, letter)
+        []
+
+        """
 
     index = []
 
@@ -244,7 +317,15 @@ def find_letter_in_word(secret_word, letter):
 
 def update_guess(guess, indexes, letter):
     """ Replaces '_' with letter at indexes of guess 
-        Returns updated guess """
+        Returns updated guess 
+
+        >>> guess = ['_']*5
+        >>> letter = 'p'
+        >>> idx = [1,2]
+        >>> update_guess(guess, idx, letter)
+        ['_', 'p', 'p', '_', '_']
+
+        """
 
     for i in indexes:
         guess[i] = letter
@@ -252,15 +333,45 @@ def update_guess(guess, indexes, letter):
     return guess
 
 def clean_data(data):
+    """Converts data to lowercase and removes extra spaces.
+        Returns cleaned value.
+
+        >>> temp = " thE "
+        >>> clean_data(temp)
+        'the'
+     
+        >>> caps = "THEIS"
+        >>> clean_data(caps)
+        'theis'
+        """ 
     
     data = data.lower()
     data = data.strip()
     return data
 
 
-def is_dupilicate(data, used):
+def is_duplicate(data, used):
     """checks to see if the entered data has been entered before.
-        Returns T if seen before. F if new."""
+        Returns T if seen before. F if new.
+
+        >>> data = 'p'
+        >>> used = ['the', 'p', 'l','a']
+        >>> is_duplicate(data, used)
+        True
+        >>> data = 'p'
+        >>> used = ['the', 'l','a']
+        >>> is_duplicate(data, used)
+        False
+        >>> data = 'the'
+        >>> used = ['the', 'p', 'l','a']
+        >>> is_duplicate(data, used)
+        True
+        >>> data = 'ham'
+        >>> used = ['the', 'p', 'l','a']
+        >>> is_duplicate(data, used)
+        False
+
+        """
 
     old = set(used)
 
@@ -271,7 +382,18 @@ def is_dupilicate(data, used):
 
 def words_match(secret_word, word):
     """Takes secret_word(as a list) word(as a string).
-        Returns True if they match. False if they dont."""
+        Returns True if they match. False if they dont.
+
+        >>> secret = ['c','a','t']
+        >>> word = 'cat'
+        >>> words_match(secret, word)
+        True
+        >>> secret = ['c','a','t']
+        >>> word = 'got'
+        >>> words_match(secret, word)
+        False
+
+        """
 
     secret = convert_to_string(secret_word)
     if secret == word:
